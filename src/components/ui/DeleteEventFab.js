a085d56e1f0b00da4762a4287a123fdd597774a5
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-05-05 23:45:59 -0400

    Abril modal para creacion o eliminacion eventos

---
+++ src/components/ui/DeleteEventFab.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { DeleteEvent } from '../../actions/events';
+
+export const DeleteEventFab = () => {
+   const { activeEvent } = useSelector(state => state.calendar);
+   const dispatch = useDispatch();
+   const handleDelete = () => {
+      dispatch(DeleteEvent());
+   }
+   return (
+      <>
+         {
+            (activeEvent) && (<button
+               className='btn btn-danger fab-danger'
+               onClick={handleDelete}>
+               <i className='fas fa-trash'></i>
+               <span> Borrar Evento</span>
+            </button>)
+         }
+      </>
+   )
+}
