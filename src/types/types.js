4e14e6c44c70f9e78ba9ebf48213ec110307b9b6
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-05-14 22:41:22 -0400

    authReducer

--- src/types/types.js
+++ src/types/types.js
@@ -5,5 +5,14 @@ export const types = {
    event: '[event] Set Active',
    eventAddNew: '[event] Add New Event',
    eventModify: '[event] Modify Event',
-   eventDelete: '[event] Delete Event'
+   eventDelete: '[event] Delete Event',
+
+   authChecking: '[auth] Checking Login User',
+   authCheckingFinish: '[auth] Finish Checking Login User',
+   authStartLogin: '[auth] Start Login User',
+   authLogin: '[auth] Login User',
+   authStartRegister: '[auth] Start Register User',
+   authStartRenewToken: '[auth] Renew User Token',
+   authLogout: '[auth] Logout User'
+
 }
